20a604673d97763a652a5afd526128ef60d8dd6f
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2021-06-29 09:29:38 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular11ImageUploadPreview';
+  title = 'Angular 11 Multiple Images Upload with Preview';
 }
